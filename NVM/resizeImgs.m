76a60e2afe5a8ce71e76456493a45476d26e9ef0
ezheng <ezheng@cs.unc.edu>
2013-05-24 20:39:09 -0400

    animate the patch match process

---
+++ NVM/resizeImgs.m
@@ -0,0 +1,43 @@
+function resizeImgs
+
+workingDir = 'C:\Enliang\data\coliseum2';
+origDir = fullfile(workingDir, 'OrignalImage');
+outputDir = fullfile(workingDir, 'image');
+if ~exist(outputDir, 'dir')
+    mkdir(outputDir);
+end
+maxDim = 1024;
+
+allFiles = dir( fullfile(origDir, '*.jpg'));
+numOfFiles = numel(allFiles);
+for i = 1:numOfFiles
+    fprintf(1,'processing image %d\n',i);
+    name = fullfile(origDir, allFiles(i).name);
+    img = imread(name);
+    
+    resultedImg = resizeImg (img, maxDim);
+    
+    outputName = fullfile(outputDir, allFiles(i).name);
+    imwrite(resultedImg, outputName);
+    
+end
+
+end
+
+
+
+
+function resultedImg = resizeImg(img, maxDim)
+    height = size(img,1);
+    width = size(img,2);
+    dim = max(height,width);
+    sz = 1;
+    while(dim > maxDim)
+        sz = sz/2;
+        dim = dim/2;        
+    end
+    resultedImg = imresize(img, sz);
+end
+
+
+
